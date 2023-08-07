ad5845e6a7dc8ab4f905a70c763eb0e4889dda2c
Janisha Patel <Janisha.Patel@softwire.com>
2023-08-07 11:31:06 +0100

    Hello World

---
+++ obj/FizzBuzz.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace FizzBuzz
+{
+        class Program {
+            static void Main(string[]args){
+                console.WriteLine("HelloWorld!");
+            }
+        }
+}
